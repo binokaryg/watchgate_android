1210f7d435356be1bec0efdee75ebe50d6d32860
Binod Adhikary <binoary@gmail.com>
2021-07-02 15:18:49 +0545

    NetworkInfo logic changed for API level >= 23 and below #3

--- app/src/main/java/com/binokary/watchgate/StatsHelper.java
+++ app/src/main/java/com/binokary/watchgate/StatsHelper.java
@@ -5,16 +5,19 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -72,13 +75,39 @@ public final class StatsHelper {
 
         //Connection
         try {
-            cm =
-                    (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-            if (activeNetwork != null) {
-                boolean isWiFi = activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
-                boolean isData = activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE;
+            cm = (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+            int result = 0; // connection type. 0: none; 1: mobile data; 2: wifi
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (cm != null) {
+                    NetworkCapabilities capabilities = cm.getNetworkCapabilities(cm.getActiveNetwork());
+                    if (capabilities != null) {
+                        if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                            result = 2;
+                        } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                            result = 1;
+                        } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
+                            result = 3;
+                        }
+                    }
+                }
+            } else {
+                if (cm != null) {
+                    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();//Deprecated: only to support below Android 6.0 (API level 23)
+                    if (activeNetwork != null) {
+                        // connected to the internet
+                        if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                            result = 2;
+                        } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                            result = 1;
+                        } else if (activeNetwork.getType() == ConnectivityManager.TYPE_VPN) {
+                            result = 3;
+                        }
+                    }
+                }
+            }
+            if (result != 0) {
+                boolean isData = result == 1;
+                boolean isWiFi = result == 2;
                 //Wifi
                 if (isWiFi) {
                     WifiManager wifiManager = (WifiManager) appContext.getApplicationContext().getSystemService(WIFI_SERVICE);
@@ -87,7 +116,7 @@ public final class StatsHelper {
                         WifiInfo info = wifiManager.getConnectionInfo();
                         if (info != null) {
                             wifi = info.getSSID().replace("\"", "");
-                            wifiSignalStrength = WifiManager.calculateSignalLevel(info.getRssi(), 5);
+                            wifiSignalStrength = calculateSignalLevelOwn(info.getRssi());
                             //Log.d(TAG, "Wifi Signal: " + wifiSignalStrength);
                             Log.d(TAG, "Wifi SSID: " + wifi);
                         }
@@ -115,9 +144,25 @@ public final class StatsHelper {
 
     public static String DateStringFromMS(long ld) {
         Date d = new Date(ld);
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
         df.setTimeZone(TimeZone.getTimeZone("Asia/Kathmandu"));
         return df.format(d);
+    }
+
+    private static int calculateSignalLevelOwn (int rssi) {
+        final int MIN_RSSI = -100;
+        final int MAX_RSSI = -55;
+        final int numLevels = 5;
+
+        if (rssi <= MIN_RSSI) {
+            return 0;
+        } else if (rssi >= MAX_RSSI) {
+            return numLevels - 1;
+        } else {
+            float inputRange = (MAX_RSSI - MIN_RSSI);
+            float outputRange = (numLevels - 1);
+            return (int)((float)(rssi - MIN_RSSI) * outputRange / inputRange);
+        }
     }
 
 }
